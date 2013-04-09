2f902d3ba37a852fdb9a62a561cbe4e324c0c37c
Bet007 <373209457@qq.com>
2013-04-09 16:49:56 +0800

    Initial Commit

---
+++ ParticleDesigner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParticleDesigner
+//
+//  Created by Bet007 on 13-4-9.
+//  Copyright (c) 2013年 ifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
